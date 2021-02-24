9e96cf8d5fcf71d722c3e937289c1e5b640e3594
Lobach Aleksandr <sanlobach789@gmail.com>
2021-02-24 22:59:25 +0300

    lesson 3 ready

---
+++ lesson_3/handler.py
@@ -0,0 +1,19 @@
+import json
+from datetime import datetime
+
+
+class MessageHandler:
+
+    @staticmethod
+    def create_msg(msg):
+        msg = json.dumps(msg)
+        return msg.encode('utf-8')
+
+    @staticmethod
+    def load_msg(data):
+        data = data.decode("utf-8")
+        try:
+            data = json.loads(data)
+        except json.decoder.JSONDecodeError:
+            data = None
+        return data
